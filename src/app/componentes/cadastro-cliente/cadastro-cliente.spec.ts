eb7dfa5023df76c2c33cc652f2576c74aa9ed54e
Pedro Henrick <pedro.h.santos507@aluno.senai.br>
2026-08-04 12:20:43 -0300

    fiz alteraçõe

---
+++ src/app/componentes/cadastro-cliente/cadastro-cliente.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CadastroCliente } from './cadastro-cliente';
+
+describe('Home', () => {
+  let component: CadastroCliente;
+  let fixture: ComponentFixture<CadastroCliente>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CadastroCliente],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CadastroCliente);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/componentes/cadastro-cliente/cadastro-cliente.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-cliente',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-cliente.html',
+  styleUrl: './cadastro-cliente.css'
+})
+export class  CadastroCliente{}
