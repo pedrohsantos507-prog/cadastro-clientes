92434472395083b66c9444d77285586a6f8a6bed
Pedro Henrick <pedro.h.santos507@aluno.senai.br>
2026-08-05 17:01:49 -0300

    adicionei novas funções ao site

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,27 @@
 import { Routes } from '@angular/router';
+import { Home } from './componentes/home/home';
+import { CadastroCliente } from './componentes/cadastro-clientes/cadastro-clientes';
+import { ListaClientes } from './componentes/lista-clientes/lista-clientes';
 
 
 export const routes: Routes = [
-{
+  {
     path: '',
-    redirectTo: 'home'
-    },
-    {
-        path:'home',
-        component: homeComponent
-    },
-    {
-        path: 'cadastro',
-        component:Formulario
-    }
-]
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+{
+    path: 'home',
+    component: Home
+},
+
+  {
+    path: 'cadastro',
+    component: CadastroCliente
+  },
+
+  {
+    path: 'consulta',
+    component: ListaClientes
+  },
+];
--- src/app/componentes/home/home.spec.ts
+++ src/app/componentes/home/home.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Menu } from './menu';
+import { Home } from './home';
 
-describe('Menu', () => {
-  let component: Menu;
-  let fixture: ComponentFixture<Menu>;
+describe('Home', () => {
+  let component: Home;
+  let fixture: ComponentFixture<Home>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Menu]
-    })
-    .compileComponents();
+      imports: [Home],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(Menu);
+    fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
