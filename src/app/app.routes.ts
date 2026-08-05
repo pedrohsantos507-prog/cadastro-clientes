9180e09deb243339cf2c6b82dcddca304aaf5b67
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-08-04 21:15:57 -0300

    adicionei novas pastas ao servidor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{
+    path: '',
+    redirectTo: 'home'
+    },
+    {
+        path:'home',
+        component: homeComponent
+    },
+    {
+        path: 'cadastro',
+        component:Formulario
+    }
+]
